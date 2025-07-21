824374912790422da0f6250451fc41da01ecfe46
MihinSen <157033905+MihinSen@users.noreply.github.com>
2025-07-21 07:13:51 +0530

    Create models.py

---
+++ controllers/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Float
+from api.dependencies.database import Base
+
+class Sandwich(Base):
+    __tablename__ = "sandwiches"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100))
+    description = Column(String(255))
+    price = Column(Float)
